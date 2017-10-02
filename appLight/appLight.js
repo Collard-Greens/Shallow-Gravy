7ee775d66e30f910036e85390f7243b12a302765
Collard-Greens <goatpickles@hotmail.com>
2017-10-02 16:44:28 -0400

    sdfsdf
    sdfsdf

--- appLight/appLight.js
+++ appLight/appLight.js
@@ -5,11 +5,14 @@ function swapClass(myDiv)
 		myDiv.className="off";
 		document.body.style.background="white";
 		myDiv.innerHTML="ON";
+		myDiv.float=left;
 	}
 	else
 	{
 		myDiv.className="on";
 		document.body.style.background="black";
 		myDiv.innerHTML="OFF";
+		myDiv.float=right;
+		
 	}
 }
