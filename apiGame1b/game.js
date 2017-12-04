f99ef6094eeab634fe1adf38f1bd799f68991245
Collard-Greens <goatpickles@hotmail.com>
2017-12-04 14:35:00 -0500

    dfgdfh
    dhaster

---
+++ apiGame1b/game.js
@@ -0,0 +1,123 @@
+"use strict";
+
+function TheApp()
+{	
+	$("#getQuestion").on("click",function()
+	{
+		
+		$.ajax({
+			url: "http://jservice.io/api/random",
+			dataType : "json"
+		})
+		
+		.done(function(data){
+			$("#Question").text(data[0].question);
+			console.log(data);
+			$("#Answer").text(data[0].answer);
+			console.log(data);
+		});
+		
+	});
+	
+	$("#getQuestion2").on("click",function()
+	{
+		
+		$.ajax({
+			url: "http://jservice.io/api/random",
+			dataType : "json"
+		})
+		
+		.done(function(data){
+			$("#Question").text(data[0].question);
+			console.log(data);
+			$("#Answer").text(data[0].answer);
+			console.log(data);
+		});
+		
+	});
+	
+	$("#getQuestion3").on("click",function()
+	{
+		
+		$.ajax({
+			url: "http://jservice.io/api/random",
+			dataType : "json"
+		})
+		
+		.done(function(data){
+			$("#Question").text(data[0].question);
+			console.log(data);
+			$("#Answer").text(data[0].answer);
+			console.log(data);
+		});
+		
+	});
+	
+	$("#getQuestion4").on("click",function()
+	{
+		
+		$.ajax({
+			url: "http://jservice.io/api/random",
+			dataType : "json"
+		})
+		
+		.done(function(data){
+			$("#Question").text(data[0].question);
+			console.log(data);
+			$("#Answer").text(data[0].answer);
+			console.log(data);
+		});
+		
+	});
+	
+	$("#getQuestion5").on("click",function()
+	{
+		
+		$.ajax({
+			url: "http://jservice.io/api/random",
+			dataType : "json"
+		})
+		
+		.done(function(data){
+			$("#Question").text(data[0].question);
+			console.log(data);
+			$("#Answer").text(data[0].answer);
+			console.log(data);
+		});
+		
+	});
+	
+	$("#getQuestion6").on("click",function()
+	{
+		
+		$.ajax({
+			url: "http://jservice.io/api/random",
+			dataType : "json"
+		})
+		
+		.done(function(data){
+			$("#Question").text(data[0].question);
+			console.log(data);
+			$("#Answer").text(data[0].answer);
+			console.log(data);
+		});
+		
+	});
+	
+	$("#getAnswer").on("click",function(){
+			$('#Answer').css("visibility", "visible");
+	});
+	
+	
+	$("#point1").on("click",function() {
+		alert("You got a point, probably. Write it down."); 
+	});
+	
+	$("#point2").on("click",function() {
+		alert("You got a point, probably. Write it down."); 
+	});
+}
+
+window.onload = function(){
+	TheApp();
+}
